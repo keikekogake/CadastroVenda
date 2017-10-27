65d62a3f87b22e758e6292d9c9b98f74dae013ad
keike.kogake <keike.kogake@gmail.com>
2017-10-27 18:58:14 -0200

    Criação dos métodos construtores da classe Produto

--- Produto.cs
+++ Produto.cs
@@ -2,6 +2,24 @@ namespace CadastroVendaPOO
 {
     public class Produto
     {
+        private int id;
+        private string nomeproduto;
+        private string descricao;
+        private double preco;
+
+        public Produto()
+        {
+
+        }
+
+        public Produto (int id, string nomeproduto, string descricao, double preco)
+        {
+            this.id = id;
+            this.nomeproduto = nomeproduto;
+            this.descricao = descricao;
+            this.preco = preco;
+        }
+
         
     }
 }
