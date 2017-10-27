aedf150cd8f11e13e440e3abc607302b547c200e
keike.kogake <keike.kogake@gmail.com>
2017-10-27 18:50:01 -0200

    Adicionado o SUMÁRIO dos métodos construtores

--- Cliente.cs
+++ Cliente.cs
@@ -9,12 +9,22 @@ namespace CadastroVendaPOO
         private string cpf;
         private DateTime datacadastro;
 
+        /// <summary>
+        /// Constroi o Construtor Cliente;
+        /// </summary>
         public Cliente()
         {
 
         }
 
-        public Cliente (string nome, string email, string cpf, DateTime datacadastro)
+        /// <summary>
+        /// Constroi o objeto Cliente e você precisa passar os dados do cliente
+        /// </summary>
+        /// <param name="nome">Recebe o NOME do Cliente como String</param>
+        /// <param name="email">Recebe o EMAIL do Cliente como String</param>
+        /// <param name="cpf">Recebe o CPF do Cliente como String</param>
+        /// <param name="datacadastro">Recebe a DATA DE CADASTRO do Cliente como DateTime</param>
+        public Cliente(string nome, string email, string cpf, DateTime datacadastro)
         {
             this.nome = nome;
             this.email = email;
@@ -24,23 +34,23 @@ namespace CadastroVendaPOO
 
         public string Nome
         {
-            get {return nome;}
-            set {nome = value;}
+            get { return nome; }
+            set { nome = value; }
         }
         public string Email
         {
-            get {return email;}
-            set {email = value;}
+            get { return email; }
+            set { email = value; }
         }
         public string Cpf
         {
-            get {return cpf;}
-            set {cpf = value;}
+            get { return cpf; }
+            set { cpf = value; }
         }
         public DateTime DataCadastro
         {
-            get {return datacadastro;}
-            set {datacadastro = value;}
+            get { return datacadastro; }
+            set { datacadastro = value; }
         }
     }
 }
