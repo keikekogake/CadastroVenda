ad2876226143c4d09c4718f19bc5b2719020b874
keike.kogake <keike.kogake@gmail.com>
2017-10-27 18:46:24 -0200

    Criação da classe Cliente.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CadastroVendaPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
